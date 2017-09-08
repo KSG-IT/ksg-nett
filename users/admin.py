526e59e648632d4b08a3fd7d5baed009ac2c5b6c
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-08 06:50:42 +0200

    Update users/models
    Renamed Person => UserProfile. This is in accordance with common
    practices. This also makes the relationship between User and
    UserProfile less ambiguous.
    
    The commit also introduces several minor updates:
        + Remove max_length from IntegerField.
        + Change some variables to all lower-case
        + Add __str__ and __repr__ methods
        + Add Meta class with default_related_name and verbose_name_plural.
        + Renamed `image` => `profile_image`. A user might have many
          pictures associated with them, and thus profile_image is more precise.
        + Fix indentation

--- users/admin.py
+++ users/admin.py
@@ -4,3 +4,6 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from users.models import UserProfile
+
+admin.site.register(UserProfile)
--- users/models.py
+++ users/models.py
@@ -1,46 +1,56 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
 
-class Person(models.Model):
-	"""
-	Model for a KSG member on KSG-nett
-	"""
+class UserProfile(models.Model):
+    """
+    Model for a KSG member on KSG-nett
+    """
+    user = models.OneToOneField(User, related_name='profile')
 
-	# Personal details
-	name = models.CharField(max_length=100)
-	date_of_birth = models.DateField()
-	study = models.CharField(max_length=100)
+    # Personal details
+    name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    study = models.CharField(max_length=100)
 
-	# Contact information
-	email = models.EmailField(max_length=50)
-	phone = models.IntegerField(max_length=8)
-	study_address = models.TextField(max_length=100)
-	home_address = models.TextField(max_length=100)
+    # Contact information
+    email = models.EmailField(max_length=50)
+    phone = models.IntegerField()
+    study_address = models.TextField(max_length=100)
+    home_address = models.TextField(max_length=100)
 
-	# KSG details
+    # KSG choices
+    KSG_ACTIVITY_TYPES = (
+        ("aktiv", "Aktiv"),
+        ("inaktiv", "Ikke aktiv"),  # Finished with KSG duties
+        ("permittert", "Permittert"),  # Implicitly inactive, wants to continue
+        ("sluttet", "Sluttet før tiden"),  # Implicitly inactive, has jumped ship
+    )
 
-	# KSG choices
-	KSG_ACTIVITY_TYPES = (
-		("aktiv", "Aktiv"),
-		("inaktiv", "Ikke aktiv"),  # Finished with KSG duties
-		("permittert", "Permittert"),  # Implicitly inactive, wants to continue
-		("sluttet", "Sluttet før tiden"),  # Implicitly inactive, has jumped ship
-	)
+    KSG_STATUS_TYPES = (
+        ("gjengis", "Gjengis"),
+        ("funk", "Funksjonær"),
+        ("hangaround", "Hangaround"),
+        ("gjengpang", "GjengPang"),
+        ("funkepang", "FunkePang"),
+        ("hospitant", "Hospitant"),
+    )
 
-	KSG_STATUS_TYPES = (
-		("gjengis", "Gjengis"),
-		("funk", "Funksjonær"),
-		("hangaround", "Hangaround"),
-		("gjengpang", "GjengPang"),
-		("funkepang", "FunkePang"),
-		("hospitant", "Hospitant"),
-	)
+    status_ksg = models.CharField(max_length=32, choices=KSG_STATUS_TYPES)
+    start_ksg = models.DateField(auto_now_add=True)
 
-	status_KSG = models.CharField(max_length=4, choices=KSG_STATUS_TYPES)
-	start_KSG = models.DateField.auto_now_add(default=timezone.now())
+    profile_image = models.FileField(upload_to='profiles/', blank=True, null=True)
 
-	image = FileField()  # (upload_to='uploads')
+    def __str__(self):
+        return "UserProfile for %s" % (self.name,)
+
+    def __repr__(self):
+        return "UserProfile(name=%s, user=%d)" % (self.name, self.user.id)
+
+    class Meta:
+        default_related_name = 'user_profiles'
+        verbose_name_plural = 'UserProfiles'
