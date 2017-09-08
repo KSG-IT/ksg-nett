abecc1dec6da8c259136f1fc295ff6093e8125cb
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-08 06:56:12 +0200

    Update settings
    Install users app. Also add local-specific placeholders for MEDIA_ROOT and STATIC_ROOT.

--- ksg_nett/settings.py
+++ ksg_nett/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
+MEDIA_ROOT = 'media/'
