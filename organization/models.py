2244a7e098c9656968cfc4e396b7d8b81cde1436
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-10 11:26:05 +0200

    Add Group model to organization
    Currently only a very bare-bones implementation.

--- organization/models.py
+++ organization/models.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+
+    members = models.ManyToManyField(
+        User,
+        blank=True,
+        related_name='internal_groups'  # The default django user model already has a `groups` related_name
+                                        # so we have to make a custom one
+    )
+
+    def __str__(self):
+        return "Group %s" % self.name
+
+    def __repr__(self):
+        return "Group(name=%s)" % self.name
+
+    class Meta:
+        default_related_name = 'groups'
+        verbose_name_plural = 'Groups'
