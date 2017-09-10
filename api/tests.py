9526d74e388946befe1f1f6465468206f7e11ad8
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-10 12:05:57 +0200

    Update api
    Add a signal-hook on user-create, to ensure all users have an associated
    token.

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
+
 # Create your tests here.
+class TokenTest(TestCase):
+    def test_create_user_should_generate_token(self):
+        user = User(
+            username='User 1'
+        )
+        user.save()
+        self.assertIsNotNone(user.auth_token)
+        self.assertIsInstance(user.auth_token.key, str)
