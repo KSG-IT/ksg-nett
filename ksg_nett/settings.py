027b1f2dc54b89ecd0b0ab5a4356aa649d231e52
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-10 12:05:07 +0200

    Add Django REST framework dependency
    This commits also enables it through INSTALLED_APPS and sets
    TokenAuthenticationa as the default authentication scheme

--- ksg_nett/settings.py
+++ ksg_nett/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'api'
 ]
 
@@ -119,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django REST framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': {
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    }
+}
+
+
