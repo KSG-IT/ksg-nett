2aa6679fd384b48a5030f89d3124e120946fa741
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-10 12:28:15 +0200

    Add api for regular Users

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,11 @@
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+
+from api.views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-
+    url(r'', include(router.urls))
 ]
