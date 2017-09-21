765eed8e374dcb1a2bb09e027adb0375aee8af7e
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-21 19:03:24 +0200

    Add new User model to the admin interface with proper forms and fieldset

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django import forms
 from django.contrib import admin
 
 # Register your models here.
-from users.models import UserProfile
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
-admin.site.register(UserProfile)
+from users.models import User
+
+
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+
+class MyUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
+    fieldsets = UserAdmin.fieldsets + (
+        ('Personalia', {'fields': ('date_of_birth', 'study',)}),
+        ('Contact', {'fields': ('phone', 'study_address', 'home_address',)}),
+    )
+
+admin.site.register(User, MyUserAdmin)
