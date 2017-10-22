03d469db42859106db4aa358458b92416dc4b3de
Tormod Haugland <tormod.haugland@gmail.com>
2017-10-22 21:13:28 +0200

    Add basic login functionality
    Add login_user view, and plain login template (without styling).

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from users.models import User
+
+
+class LoginViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        user = User(username='user')
+        user.set_password('password')
+        user.save()
+
+    def test_plain_user_login(self):
+        response = self.client.post('/login/', {
+            'username': 'user',
+            'password': 'password'
+        })
+
+        self.assertEqual(response.status_code, 302)
+        cookies = list(map(lambda x: x[0], response.client.cookies.items()))
+        self.assertIn('sessionid', cookies)
+
--- login/views.py
+++ login/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from rest_framework import status
 
-# Create your views here.
+
+def login_user(request):
+    """
+    Logs a user in.
+
+    :type request: django.http.HttpRequest
+    :return:
+    """
+    requested_next_url = request.GET.get('next')
+
+    # We are already authenticated.
+    if request.user.is_authenticated:
+        if requested_next_url:
+            return redirect(requested_next_url)
+        # TODO: Redirect internal when internal module merges
+        return redirect('/')
+
+    if request.method == 'GET':
+        return render(request, 'login/login.html')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return render(request, 'login/login.html', {'errors': 'Bad username or password'})
+        else:
+            login(request, user)
+            if requested_next_url:
+                return redirect(requested_next_url, context={'user': user})
+            # TODO: Redirect internal when internal module merges
+            return redirect('/', context={'user': user})
+
+    return HttpResponse(status=status.HTTP_401_UNAUTHORIZED)
+
+
+def logout_user(request):
+    pass
