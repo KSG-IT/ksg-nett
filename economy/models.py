61acecac7da52c7e837a310de110a572b8fb9e4d
lisamso <lisasoem@live.no>
2017-11-14 19:33:33 +0100

    Changed name of CrossIn to RegisteredCross and changed person fields to be foreign keys to User.

--- economy/models.py
+++ economy/models.py
@@ -56,18 +56,18 @@ class Product(models.Model):
         return "A product with name %s with the price of %d NOK" %(self.product_name, self.price)
 
 
-class CrossIn(models.Model):
+class RegisteredCross(models.Model):
     date_crossed = models.DateField(blank=False, null=False)
     date_registered = models.DateField(auto_now_add=True, blank=False, null=False)
-    person_crossing_in = models.CharField(max_length=100, blank=False, null=False)
+    person_crossing_in = models.ForeignKey(User, blank=False, null=False)
     comment = models.CharField(max_length=100, blank=False, null=False)
 
 
 class Cross(models.Model):
-    person = models.CharField(max_length=100, blank=True, null=True)
+    person = models.ForeignKey(User, blank=True, null=True)
     product = models.ForeignKey(Product, blank=False, null=False)
     amount = models.IntegerField(blank=False, null=False)
-    cross_in = models.ForeignKey(CrossIn, on_delete=models.CASCADE)
+    cross_in = models.ForeignKey(RegisteredCross, on_delete=models.CASCADE)
 
 
 
