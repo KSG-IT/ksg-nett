301fa8289eafbe6dd3274d124593f095f53ab7a6
lisamso <lisasoem@live.no>
2017-11-16 18:23:44 +0100

    Added creation test for Product

--- economy/tests.py
+++ economy/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from economy.models import Product, Deposit
+from users.models import User
 
-# Create your tests here.
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        Product.objects.create(product_name='burger', price=50)
+        Product.objects.create(product_name='flaskepils', price=25)
+
+    def test_creation(self):
+        self.assertIsInstance(Product.objects.get('burger'), Product)
+        self.assertIsInstance(Product.objects.get('flaskepils'), Product)
+
