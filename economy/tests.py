de98033ecb61748b5ef755f4a7aca0f1b6e3edc3
lisamso <lisasoem@live.no>
2017-11-16 19:21:36 +0100

    Added tests for Deposit.

--- economy/tests.py
+++ economy/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from economy.models import Product, Deposit
 from users.models import User
+import datetime
 
 
 class ProductTestCase(TestCase):
@@ -23,5 +24,24 @@ class ProductTestCase(TestCase):
 class DepositTestCase(TestCase):
 
     def setUp(self):
-        self.user1 = User.objects.create(id=1, email='person1@something.com')
-        self.user2 = User.objects.create(id=2, email='person2@something.com')
+        self.user1 = User.objects.create(id=1, username='user1', email='person1@something.com')
+        self.user2 = User.objects.create(id=2, username='user2', email='person2@something.com')
+        self.deposit = Deposit.objects.create(person=self.user1, amount=100)
+        self.now = datetime.datetime.now()
+
+    def test_creation(self):
+        self.assertIsInstance(self.deposit, Deposit)
+
+    def test_time_of_creation(self):
+        self.assertEquals(self.now.date(), self.deposit.signed_off_time.date())
+        self.assertEquals(self.now.hour, self.deposit.signed_off_time.hour + 1) # +1 to set to norwegian time zone
+        self.assertEquals(self.now.minute, self.deposit.signed_off_time.minute)
+        self.assertEquals(self.now.second, self.deposit.signed_off_time.second)
+        self.assertEquals(self.now.microsecond, self.deposit.signed_off_time.microsecond)
+
+    def test_signed_off_by(self):
+        self.assertIsNone(self.deposit.signed_off_by)
+        self.deposit.signed_off_by = self.user2
+        self.assertEquals(self.user2, self.deposit.signed_off_by)
+
+
