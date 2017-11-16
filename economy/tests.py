d864701d89177e4038ec1f06063c53dd120a0599
lisamso <lisasoem@live.no>
2017-11-16 18:47:44 +0100

    Added tests for Product.__str__

--- economy/tests.py
+++ economy/tests.py
@@ -13,9 +13,15 @@ class ProductTestCase(TestCase):
         self.assertIsInstance(self.burger, Product)
         self.assertIsInstance(self.pils, Product)
 
+    def test_to_string(self):
+        string = self.burger.__str__()
+        self.assertEquals(string, 'A product with name burger with the price of 50 NOK')
+        string = self.pils.__str__()
+        self.assertEquals(string, 'A product with name flaskepils with the price of 25 NOK')
+
 
 class DepositTestCase(TestCase):
 
     def setUp(self):
-        User.objects.create(id=1, email='person1@something.com')
-        User.objects.create(id=2, email='person2@something.com')
+        self.user1 = User.objects.create(id=1, email='person1@something.com')
+        self.user2 = User.objects.create(id=2, email='person2@something.com')
