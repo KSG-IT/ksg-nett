9350e2bd467d3d29b2d332d5b84f646e93175eae
lisamso <lisasoem@live.no>
2017-11-16 19:45:36 +0100

    Added tests for Transaction.

--- economy/tests.py
+++ economy/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from economy.models import Product, Deposit
+from economy.models import Product, Deposit, Transaction
 from users.models import User
 import datetime
 
@@ -37,11 +37,41 @@ class DepositTestCase(TestCase):
         self.assertEquals(self.now.hour, self.deposit.signed_off_time.hour + 1) # +1 to set to norwegian time zone
         self.assertEquals(self.now.minute, self.deposit.signed_off_time.minute)
         self.assertEquals(self.now.second, self.deposit.signed_off_time.second)
-        self.assertEquals(self.now.microsecond, self.deposit.signed_off_time.microsecond)
 
     def test_signed_off_by(self):
         self.assertIsNone(self.deposit.signed_off_by)
         self.deposit.signed_off_by = self.user2
         self.assertEquals(self.user2, self.deposit.signed_off_by)
+        self.assertNotEqual(self.deposit.person, self.deposit.signed_off_by)
 
 
+class TransactionTestCase(TestCase):
+
+    def setUp(self):
+        self.user1 = User.objects.create(id=1, username='user1', email='person1@something.com')
+        self.user2 = User.objects.create(id=2, username='user2', email='person2@something.com')
+        self.user3 = User.objects.create(id=3, username='user3', email='person3@something.com')
+        self.transaction = Transaction.objects.create(sender=self.user1,
+                                                      recipient=self.user2,
+                                                      amount=100)
+        self.now = datetime.datetime.now()
+
+    def test_creation(self):
+        self.assertIsInstance(self.transaction, Transaction)
+
+    def test_time_of_creation(self):
+        self.assertEquals(self.now.date(), self.transaction.signed_off_time.date())
+        self.assertEquals(self.now.hour, self.transaction.signed_off_time.hour + 1) # +1 to set to norwegian time zone
+        self.assertEquals(self.now.minute, self.transaction.signed_off_time.minute)
+        self.assertEquals(self.now.second, self.transaction.signed_off_time.second)
+
+    def test_signed_off_by(self):
+        self.assertIsNone(self.transaction.signed_off_by)
+        self.transaction.signed_off_by = self.user3
+        self.assertEquals(self.user3, self.transaction.signed_off_by)
+        self.assertNotEqual(self.transaction.sender, self.transaction.signed_off_by)
+        self.assertNotEqual(self.transaction.recipient, self.transaction.signed_off_by)
+
+    def test_to_string(self):
+        string = 'Transaction from user1 to user2 of 100 NOK'
+        self.assertEquals(self.transaction.__str__(), string)
