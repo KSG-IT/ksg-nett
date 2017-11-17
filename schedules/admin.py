614eac9b7db51f054d90dd461633256aa8a8a698
Tormod Haugland <tormod.haugland@gmail.com>
2017-11-17 10:36:31 +0100

    Add schedules models to admin site

--- schedules/admin.py
+++ schedules/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from schedules.models import Schedule, ShiftSlotGroupInterest, ShiftTradeOffer, ShiftTrade, ShiftSlotTemplate, \
+    ShiftSlotDayRule, ShiftSlotGroupTemplate, ScheduleTemplate, Shift, ShiftSlot, ScheduleSlotType, ShiftSlotGroup
+
+admin.site.register(Schedule)
+admin.site.register(ShiftSlotGroup)
+admin.site.register(ScheduleSlotType)
+admin.site.register(ShiftSlot)
+admin.site.register(Shift)
+admin.site.register(ScheduleTemplate)
+admin.site.register(ShiftSlotGroupTemplate)
+admin.site.register(ShiftSlotDayRule)
+admin.site.register(ShiftSlotTemplate)
+admin.site.register(ShiftTrade)
+admin.site.register(ShiftTradeOffer)
+admin.site.register(ShiftSlotGroupInterest)
