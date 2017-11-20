65d9a34d2366e3e93913eb2a90fd862d673b4d1c
Tormod Haugland <tormod.haugland@gmail.com>
2017-11-20 11:58:17 +0100

    Add Quote and QuoteVote models

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.db.models import Sum, Index
 
-# Create your models here.
+from users.models import User
+
+
+class Quote(models.Model):
+    text = models.TextField()
+    quoter = models.ForeignKey(User, related_name='quotes')
+    # None indicates not validated
+    verified_by = models.ForeignKey(User, null=True, related_name='verified_quotes')
+
+    @property
+    def sum(self):
+        """
+        Get the sum of votes on this quote. As quote-votes have values 1 or -1 we
+        can simply aggregate the value field.
+        :return Int:
+        """
+        return self.votes.aggregate(value=Sum('value'))['value']
+
+    def __str__(self):
+        return "Quote by %s" % (self.quoter.first_name,)
+
+    def __repr__(self):
+        return "Quote(text=%s,quoter=%s)" % (self.text, self.quoter.first_name,)
+
+    class Meta:
+        verbose_name_plural = 'quotes'
+
+
+class QuoteVote(models.Model):
+    quote = models.ForeignKey(Quote, related_name='votes')
+    # 1 for thumbs up, -1 for thumbs down. Having the field as an integer field
+    # allows for future flexibility such as allowing for extra-value thumbs-up etc.
+    # We can also get the total tally now by aggregating the sum of this column.
+    value = models.SmallIntegerField()
+    caster = models.ForeignKey(User)
+
+    class Meta:
+        indexes = [
+            # We index on quote, as we will have to filter on a specific quote
+            # on every hit to the quote from our APIs needing the fully vote-tally.
+            Index(fields=['quote'])
+        ]
+
+        verbose_name_plural = 'quote votes'
+
+    def __str__(self):
+        if self.value > 0:
+            return "Up-vote from %s to quote by %s" % (self.caster.first_name, self.quote.quoter.first_name,)
+        else:
+            return "Down-vote from %s to quote by %s" % (self.caster.first_name, self.quote.quoter.first_name,)
+
+    def __repr__(self):
+        return "QuoteVote(quote=%d,value=%d,caster=%s)" % (
+            self.quote_id,
+            self.value,
+            self.caster.first_name
+        )
