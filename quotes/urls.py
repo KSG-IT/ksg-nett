046f172030e9e50c04efe13e6a58c16d9d5018dc
Tormod Haugland <tormod.haugland@gmail.com>
2018-01-18 15:18:43 +0100

    Update quotes/urls
    Fix an issue with the regex determining the relevant quote for vote-up and vote-down. The regex didn't allow for multiple digits.

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from quotes import views
 
 urlpatterns = [
-    url('(?P<quote_id>[0-9])/vote-up', views.vote_up),
-    url('(?P<quote_id>[0-9])/vote-down', views.vote_down),
+    url('(?P<quote_id>[0-9]+)/vote-up', views.vote_up),
+    url('(?P<quote_id>[0-9]+)/vote-down', views.vote_down),
     url('^', views.list_view),
 ]
