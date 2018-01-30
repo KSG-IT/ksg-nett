b1b21d17ca8fa91c30f9e09c3e9cbdec6972b269
Tormod Haugland <tormod.haugland@gmail.com>
2018-01-30 18:02:41 +0100

    Update quotes/urls
    Fix typo in url pattern

--- quotes/urls.py
+++ quotes/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url('(?P<quote_id>[0-9]+)/vote-down', views.vote_down),
     url('^add', views.quotes_add),
     url('(?P<quote_id>[0-9]+)/edit', views.quotes_edit),
-    url('(?P<quote_id>[0-9]+)/delete', views.quotes_edit),
+    url('(?P<quote_id>[0-9]+)/delete', views.quotes_delete),
     url('^', views.quotes_list),
 ]
