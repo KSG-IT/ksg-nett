ec17ef17860c93acb79bb6eab79924b6484b9e2d
Tormod Haugland <tormod.haugland@gmail.com>
2018-01-31 08:37:56 +0100

    Set up default csrf token for axios client

--- internal/static/internal/js/main.js
+++ internal/static/internal/js/main.js
@@ -1,3 +1,4 @@
+/* globals axios */
 // noinspection JSUnusedGlobalSymbols
 
 /**
@@ -6,3 +7,8 @@
 function getCsrfToken(){
     return document.querySelector('meta[name="csrf-token"]').getAttribute('content')
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+    // Set up axios with proper defaults
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = getCsrfToken();
+});
