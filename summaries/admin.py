ee7502f9c4b20e1a12dd66b2f0aa1e49cb9b192c
Tormod Haugland <tormod.haugland@gmail.com>
2018-02-16 08:00:37 +0100

    Add Summary to Admin and add SummaryForm

--- summaries/admin.py
+++ summaries/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from summaries.models import Summary
+
+admin.site.register(Summary)
---
+++ summaries/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from summaries.models import Summary
+
+
+class SummaryForm(forms.ModelForm):
+    class Meta:
+        model = Summary
+        fields = (
+            'summary_type', 'contents', 'participants', 'reporter'
+        )
