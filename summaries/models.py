e0d6999dcb524295ee126c03fd69dbbd7c2a01cf
Tormod Haugland <tormod.haugland@gmail.com>
2018-11-01 21:17:23 +0100

    Update Summary model
    Add get_short_summary_type_name helper.
    The method renders the short name version of the
    summary type. This is typically just the regular long
    name, minus the "summary" suffix.

--- summaries/models.py
+++ summaries/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-
 # Create your models here.
-from summaries.consts import SUMMARY_TYPE_CHOICES, SUMMARY_TYPE_OTHER, SUMMARY_TYPE_CHOICES_DICT
+from summaries.consts import SUMMARY_TYPE_CHOICES, SUMMARY_TYPE_OTHER, SUMMARY_TYPE_CHOICES_DICT, \
+    SUMMARY_TYPE_SHORT_NAMES_DICT
 from users.models import User
 
 
@@ -27,6 +27,9 @@ class Summary(models.Model):
         related_name='reported_summaries'
     )
     date = models.DateTimeField(null=False, blank=False)
+
+    def get_short_summary_type_name(self):
+        return SUMMARY_TYPE_SHORT_NAMES_DICT.get(self.summary_type)
 
     def __str__(self):
         return "%s at %s" % (
