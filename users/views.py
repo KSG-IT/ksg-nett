de5480d28b998ac39d0dfafd5e40c8d0014ab102
Tormod Haugland <tormod.haugland@gmail.com>
2018-11-01 21:25:54 +0100

    Add a basic profile page

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+
+from users.models import User
 
 
 def current_user(request):
@@ -9,3 +12,12 @@ def current_user(request):
     return render(request=request,
                   template_name='users/profile_page.html',
                   context=user)
+
+
+@login_required
+def user_detail(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    ctx = {
+        'user': user
+    }
+    return render(request, template_name='users/profile_page.html', context=ctx)
