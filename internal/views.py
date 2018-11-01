2fa54eb7dc2a7597f7a370bf345ad0c652b765f0
Tormod Haugland <tormod.haugland@gmail.com>
2018-11-01 21:24:50 +0100

    Implement a basic frontpage

--- internal/views.py
+++ internal/views.py
@@ -1,7 +1,23 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from quotes.models import Quote
+from summaries.models import Summary
+
 
 @login_required
 def index(request):
-    return render(request, 'internal/base.html')
+
+    last_summaries = Summary.objects.order_by('-date')[0:10]
+    last_quotes = Quote.verified_objects.order_by('-created_at')[0:10]
+
+    ctx = {
+        'last_summaries': last_summaries,
+        'last_quotes': last_quotes
+    }
+    return render(request, 'internal/frontpage.html', context=ctx)
+
+
+@login_required
+def not_found(request):
+    return render(request, 'internal/not_found.html')
