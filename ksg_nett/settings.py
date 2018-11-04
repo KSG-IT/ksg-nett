88735be05abf609a350e44ec34a19686d6d56344
Tormod Haugland <tormod.haugland@gmail.com>
2018-11-04 14:19:47 +0100

    Update settings.py
    Add a default location for locales. This makes it possible
    for us to start experimenting with  i18n.

--- ksg_nett/settings.py
+++ ksg_nett/settings.py
@@ -140,6 +140,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Override in production
+LOCALE_PATHS = ['locales/',]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
