3c4ff34b94b9cdb94fb23436035d4d6e7d711643
Tormod Haugland <tormod.haugland@gmail.com>
2019-01-12 17:23:35 +0100

    [Summaries] Update urls to use `path` instead of `url`

--- summaries/urls.py
+++ summaries/urls.py
@@ -1,15 +1,14 @@
-from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.summaries_list),
+    re_path(r'^$', views.summaries_list),
     path('create', views.summaries_create, name='summaries_create'),
     path('search', views.summaries_search, name='summaries_search'),
     path('last', views.summaries_last, name='summaries_last'),
     path('archive', views.summaries_archive, name='summaries_archive'),
-    url(r'(?P<summary_id>[0-9]+)/update', views.summaries_update, name='summaries_update'),
-    url(r'(?P<summary_id>[0-9]+)/delete', views.summaries_delete, name='summaries_delete'),
-    url(r'(?P<summary_id>[0-9]+)', views.summaries_detail, name='summaries_detail'),
+    path('<int:summary_id>/update', views.summaries_update, name='summaries_update'),
+    path('<int:summary_id>/delete', views.summaries_delete, name='summaries_delete'),
+    path('<int:summary_id>', views.summaries_detail, name='summaries_detail'),
 ]
