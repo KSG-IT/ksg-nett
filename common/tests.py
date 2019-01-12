8c359603f1183a8e09a6502ecae185196ffc055f
Tormod Haugland <tormod.haugland@gmail.com>
2019-01-12 22:05:37 +0100

    Add utility method for extracting ranges of semester-year shorthands

--- common/tests.py
+++ common/tests.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+from unittest.mock import patch
+
 from django.template import Template, Context
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from common.util import get_semester_year_shorthand
+from common.util import get_semester_year_shorthand, get_semester_year_shorthands_by_count, \
+    get_semester_year_shorthands_by_date
 from common.views import index
 from users.models import User
 from internal.views import index as internal_index
@@ -78,4 +82,101 @@ class TestGetSemesterYearShortHandFilter(TestCase):
         with self.assertRaises(ValueError):
             context = Context({'timestamp': []})
             output = self.template.render(context)
+
+
+class TestGetSemesterYearShorthandsByDate(TestCase):
+    def get_semester_year_shorthands_by_date__argument_is_in_this_semester__returns_array_with_this_semester_only(self):
+        # Patch timezone now, so we can make reliable tests without re-implementing the
+        # method itself in the test
+        with patch(
+                'django.utils.timezone.now',
+                return_value=datetime(2018, 1, 1, tzinfo=timezone.utc)
+        ):
+            date = timezone.datetime(2018, 1, 1, tzinfo=timezone.utc)
+            results = get_semester_year_shorthands_by_date(date)
+            self.assertListEqual(
+                results,
+                ["H18"]
+            )
+
+    def get_semester_year_shorthands_by_date__argument_is_5_semesters_back__returns_correct_result(self):
+        with patch(
+                'django.utils.timezone.now',
+                return_value=datetime(2018, 1, 1, tzinfo=timezone.utc)
+        ):
+            date = timezone.datetime(2016, 1, 1, tzinfo=timezone.utc)
+            results = get_semester_year_shorthands_by_date(date)
+            self.assertListEqual(
+                results,
+                ["V18", "H17", "V17", "H16", "V16"]
+            )
+
+    def get_semester_year_shorthands_by_date__argument_is_in_the_future__returns_empty_list(self):
+        with patch(
+                'django.utils.timezone.now',
+                return_value=datetime(2018, 1, 1, tzinfo=timezone.utc)
+        ):
+            date = timezone.datetime(2019, 1, 1, tzinfo=timezone.utc)
+            results = get_semester_year_shorthands_by_date(date)
+            self.assertListEqual(
+                results,
+                []
+            )
+
+    def get_semester_year_shorthands_by_date__argument_is_around_century_change__returns_correct_result(self):
+        with patch(
+                'django.utils.timezone.now',
+                return_value=datetime(2001, 1, 1, tzinfo=timezone.utc)
+        ):
+            date = timezone.datetime(1999, 1, 1, tzinfo=timezone.utc)
+            results = get_semester_year_shorthands_by_date(date)
+            self.assertListEqual(
+                results,
+                ["V01", "H00", "V00", "H99", "V99"]
+            )
+
+
+class TestGetSemesterYearShorthandsByCount(TestCase):
+
+    def get_semester_year_shorthands_by_count__argument_is_one__returns_correct_result(self):
+        # Patch timezone now, so we can make reliable tests without re-implementing the
+        # method itself in the test
+        with patch(
+                'django.utils.timezone.now',
+                return_value=datetime(2018, 1, 1, tzinfo=timezone.utc)
+        ):
+            results = get_semester_year_shorthands_by_count(1)
+            self.assertListEqual(
+                results,
+                ["V18"]
+            )
+
+    def get_semester_year_shorthands_by_count__regular_positive_integer__returns_correct_result(self):
+        with patch(
+            'django.utils.timezone.now',
+            return_value=datetime(2018, 1, 1, tzinfo=timezone.utc)
+        ):
+            results = get_semester_year_shorthands_by_count(5)
+            self.assertListEqual(
+                results,
+                ["V18", "H17", "V17", "H16", "V16"]
+            )
+
+    def get_semester_year_shorthands_by_count__negative_integer__returns_empty_list(self):
+        results = get_semester_year_shorthands_by_count(-1)
+        self.assertListEqual(results, [])
+
+    def get_semester_year_shorthands_by_count__timezone_now_around_century_change__returns_correct_result(self):
+        # This test simultaneously tests that we can render numbers 0-9 with leading zeros
+        # and that we handle the century change.
+
+        with patch(
+                'django.utils.timezone.now',
+                return_value=datetime(2001, 1, 1, tzinfo=timezone.utc)
+        ):
+            results = get_semester_year_shorthands_by_count(5)
+            self.assertListEqual(
+                results,
+                ["V01", "H00", "V00", "H99", "V99"]
+            )
 
--- common/util.py
+++ common/util.py
@@ -1,5 +1,7 @@
 from datetime import datetime, date
-from typing import Union
+from typing import Union, List
+
+from django.utils import timezone
 
 
 def get_semester_year_shorthand(timestamp: Union[datetime, date]) -> str:
@@ -14,3 +16,76 @@ def get_semester_year_shorthand(timestamp: Union[datetime, date]) -> str:
     short_year_format = str(timestamp.year)[2:]
     semester_prefix = "H" if timestamp.month > 7 else "V"
     return f"{semester_prefix}{short_year_format}"
+
+
+def get_semester_year_shorthands_by_date(timestamp: Union[datetime, date]) -> List[str]:
+    """
+    get_semester_year_shorthands_by_date gets a list of semester-year shorthands backwards
+    in time. The earliest shorthand is defined by the semester the given timestamp occurs in.
+
+    Example:
+        timestamp = 12/04/2015, timezone.now → 25/05/2018
+        result: [
+            "V18",
+            "H17",
+            "V17",
+            "H16",
+            "V16",
+            "H15",
+            "V15"
+        ]
+
+    :param timestamp:
+    :return:
+    """
+    now = timezone.now()
+    current_semester_shorthand = get_semester_year_shorthand(now)
+    target_semester_shorthand = get_semester_year_shorthand(timestamp)
+
+    semester, year = current_semester_shorthand[0], int(current_semester_shorthand[1:])
+    target_semester, target_year = target_semester_shorthand[0], int(target_semester_shorthand[1:])
+
+    if timestamp.year > now.year or (timestamp.year == now.year and target_semester < semester):  # H < V alphabetically, and hence in the comparison
+        return []
+
+    results = [current_semester_shorthand]
+    while semester != target_semester or year != target_year:
+        year = year if semester == "H" else 99 if year == 0 else year - 1
+        semester = "V" if semester == "H" else "H"
+        results.append(
+            f"{semester}{str(year).zfill(2)}"
+        )
+
+    return results
+
+
+def get_semester_year_shorthands_by_count(number: int) -> List[str]:
+    """
+    get_semester_year_shorthands_by_count returns `number` amount of semester-year shorthands
+    backwards in time.
+
+    Example:
+        number = 3, timezone.now → 25/05/2018
+        result: ["V18", "H17", "V17"]
+
+    :param number:
+    :return:
+    """
+    if number <= 0:
+        return []
+
+    current_semester_shorthand = get_semester_year_shorthand(timezone.now())
+    semester, year = current_semester_shorthand[0], int(current_semester_shorthand[1:])
+
+    results = [current_semester_shorthand]
+
+    while number > 1:
+        number -= 1
+        year = year if semester == "H" else 99 if year == 0 else year - 1
+        semester = "V" if semester == "H" else "H"
+        results.append(
+            f"{semester}{str(year).zfill(2)}"
+        )
+
+    return results
+
