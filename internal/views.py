357071fdcb654de8f19120c9b1b53868554d93ab
Alexander Orvik <alexaor@stud.ntnu.no>
2019-07-22 16:26:32 +0200

    Add missing information to be rendered in the context
    Adds missing lists for economy and shifts such that
    one can render the latest dxeposits/purchases and upcoming
    shifts throught the context on the front end

--- internal/views.py
+++ internal/views.py
@@ -1,19 +1,29 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
+from django.utils import timezone
 from quotes.models import Quote
 from summaries.models import Summary
+from itertools import chain
 
 
 @login_required
 def index(request):
     last_summaries = Summary.objects.order_by('-date')[0:10]
     last_quotes = Quote.verified_objects.all().order_by('-created')[0:10]
-
+    economy_purchases = request.user.bank_account.purchases.all()[:10]
+    economy_deposits = request.user.verified_deposits.all()[:10]
+    # chains the two lists sorted by date and then we can slice the latest history of the two items
+    result_list = sorted(
+        chain(economy_deposits, economy_purchases),
+        key=lambda instance: instance.created
+    )
     ctx = {
         'last_summaries': last_summaries,
-        'last_quotes': last_quotes
+        'last_quotes': last_quotes,
+        'economy:': result_list[:10],
+        'next_shifts': request.user.shift_set.filter(slot__group__meet_time__gte=timezone.now())[:2],
     }
+    print(result_list)
     return render(request, 'internal/frontpage.html', context=ctx)
 
 
