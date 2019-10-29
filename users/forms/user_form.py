d95975b235429ee4302846f804411f8969c0556a
Alexander Orvik <alexaor@stud.ntnu.no>
2019-10-29 20:05:10 +0100

    Add labels field to UserForm for label rendering in template
    Allows to access related labels in UserForm which keeps
    the template free of some logic, can be extended to
    render all fields through a loop instead at a later
    stage

--- users/forms/user_form.py
+++ users/forms/user_form.py
@@ -1,11 +1,27 @@
 from django import forms
 from users.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = {
-            'first_name', 'last_name', 'phone', 'study', 'biography', 'email', 'profile_image', 'study_address',
+            'first_name', 'last_name', 'phone', 'study', 'biography', 'email', 'study_address',
             'home_address', 'commission', 'ksg_status', 'in_relationship'
+        }
+        labels = {
+            'first_name': _('First name'),
+            'last_name': _('Last name'),
+            'phone': _('Phone'),
+            'study': _('Field of study'),
+            'biography': _('Biography'),
+            'email': _('Email'),
+            'study_address': _('Study address'),
+            'home_address': _('Home address'),
+            'commission': _('Commission'),
+            'ksg_status': _('KSG status'),
+            'in_relationship': _('In relationship')
+
+
         }
