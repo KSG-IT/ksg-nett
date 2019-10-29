5e7938c15c82c8d8980bed7ac445a0bdc86a17c3
Alexander Orvik <alexaor@stud.ntnu.no>
2019-10-29 18:12:55 +0100

    Add missing fields from user model to UserForm

--- users/forms/user_form.py
+++ users/forms/user_form.py
@@ -6,5 +6,6 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = {
-            'first_name', 'last_name', 'phone', 'study', 'biography', 'email'
+            'first_name', 'last_name', 'phone', 'study', 'biography', 'email', 'profile_image', 'study_address',
+            'home_address', 'commission', 'ksg_status', 'in_relationship'
         }
