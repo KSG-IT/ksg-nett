35a19e7942d5098da954ae33b9872183bea0ba81
Alexander Orvik <alexaor@stud.ntnu.no>
2019-12-09 18:32:54 +0100

    Trim out redunant settings in production settings

--- ksg_nett/settings_prod.py
+++ ksg_nett/settings_prod.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rc4yscfoc9loe+937$q-57agxy0iq+!o0zowl0#vylilol2-)e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".alexanderorvik.com"]
 
@@ -28,65 +28,11 @@ DATABASES = {
 # S3 media files
 AWS_REGION = "eu-west-3"
 AWS_S3_BUCKET_AUTH = False
-
 AWS_S3_BUCKET_NAME = "ksg-nett-bucket"
 DEFAULT_FILE_STORAGE = "django_s3_storage.storage.S3Storage"
 MEDIA_URL = f"https://{AWS_S3_BUCKET_NAME}.s3.amazonaws.com/"
 
-# Password validation
-
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
-# TODO: In case you didn't know, 6 characters is not enough nowadays...
-# TODO: This should obviously be changed before production
-
-if not DEBUG:
-    AUTH_PASSWORD_VALIDATORS += [
-        {
-            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-        },
-    ]
 # Override in production
 LOCALE_PATHS = ['locales/', ]
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-# API DOCS
-# ------------------------------
-SWAGGER_SETTINGS = {
-    'DEFAULT_AUTO_SCHEMA_CLASS': 'api.api_docs.CustomSwaggerAutoSchema',
-}
-
-REDOC_SETTINGS = {
-    'PATH_IN_MIDDLE': True,
-    'REQUIRED_PROPS_FIRST': True
-}
-
-# ECONOMY SETTINGS
-# ------------------------------
-SOCI_MASTER_ACCOUNT_CARD_ID = 0xBADCAFEBABE  # Real card ids are 10 digits, while this is 14, meaning no collisions
-DIRECT_CHARGE_SKU = "X-BELOP"
-
-
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("localhost", 6379)]
-        }
-    }
-}
-
-
-# Redis
-REDIS = {
-    'host': 'localhost',
-    'port': 6379
-}
-CHAT_STATE_REDIS_DB = 1
 
 
