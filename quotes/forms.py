4d6619912039c731af8e224ca4c89dfcabfa5f29
Alexander Orvik <alexaor@stud.ntnu.no>
2019-12-27 20:41:18 +0100

    Add css class to QuoteForm __init__ method to style field inputs
    This addition allows one to add individual form input field styling
    by appending css classes to the field widget

--- quotes/forms.py
+++ quotes/forms.py
@@ -8,3 +8,8 @@ class QuoteForm(forms.ModelForm):
         model = Quote
         fields = ('text', 'quoter')
 
+    # This adds the mentioned css class to the related field widget which allows for more custom styling.
+    # Can accept several classes separated with space
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].widget.attrs.update({'class': 'default-text-area'})
