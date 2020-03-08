1c3c9761ff222b4d75bd6104f0e9dc8fa940d3db
Magnus Lauritzen Holtet <magnulho@gmail.com>
2020-03-08 18:36:42 +0100

    Ensure signal handler does not attempt to create a new soci bank account if the user already has one.

--- users/signals.py
+++ users/signals.py
@@ -6,6 +6,8 @@ from economy.models import SociBankAccount
 
 
 @receiver(signals.post_save, sender=User)
-def create_soci_bank_account(sender, instance, created, **kwargs):
-    if created:
+def create_soci_bank_account(sender, instance: User, created, **kwargs):
+    # We use getattr here as accessing the field directly when the relation does not
+    # exist will throw an error.
+    if created and getattr(instance, "bank_account", None) is not None:
         SociBankAccount.objects.create(user=instance)
