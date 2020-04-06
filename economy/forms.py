3d7ff15d3fb5366808b125219bb2f1a3ace9f72f
Alexander Orvik <alexaor@stud.ntnu.no>
2020-04-07 00:58:45 +0200

    Add ProductOrder model form

--- economy/forms.py
+++ economy/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-from economy.models import Deposit, DepositComment, SociProduct
-
+from economy.models import Deposit, DepositComment, SociProduct, ProductOrder
 
 class DepositForm(forms.ModelForm):
     class Meta:
@@ -18,3 +17,9 @@ class SociProductForm(forms.ModelForm):
     class Meta:
         model = SociProduct
         fields = ("sku_number", "name", "price", "description", "icon")
+
+
+class ProductOrderForm(forms.ModelForm):
+    class Meta:
+        model = ProductOrder
+        fields = ("source", "order_size", "product")
