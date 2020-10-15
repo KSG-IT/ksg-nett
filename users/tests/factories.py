641c45b3e41652a07a4cb96886d4c747d3deff62
Tormod Haugland <tormod.haugland@gmail.com>
2020-10-15 18:54:35 +0200

    fix: Update a large number of tests

--- users/tests/factories.py
+++ users/tests/factories.py
@@ -13,30 +13,26 @@ class UserFactory(DjangoModelFactory):
         model = User
 
     # From AbstractUser model
-    username = Sequence(lambda n: Faker('user_name').evaluate(None, None, {}) + str(n))
-    first_name = Faker('first_name')
-    last_name = Faker('last_name')
+    username = Sequence(lambda n: f"username{n}")
+    first_name = Faker("first_name")
+    last_name = Faker("last_name")
 
     # From User model
-    date_of_birth = Faker('past_date')
-    study = Faker('job')
+    date_of_birth = Faker("past_date")
+    study = Faker("job")
     profile_image = FileField()
-    phone = Faker('phone_number')
-    study_address = Faker('address')
-    home_address = Faker('address')
-    start_ksg = Faker('past_date')
+    phone = Faker("phone_number")
+    study_address = Faker("address")
+    home_address = Faker("address")
+    start_ksg = Faker("past_date")
     ksg_status = random.choice(list(KSG_STATUS_TYPES))[0]
     ksg_role = random.choice(list(KSG_ROLES))[0]
-    biography = Faker('sentence')
+    biography = Faker("sentence")
     in_relationship = False
     commission = SubFactory(CommissionFactory)
     anonymize_in_made_out_map = False
 
-    @sequence
-    def email(n):
-        first_part, last_part = Faker('email').evaluate(None, None, {}).split('@')
-        return first_part + str(n) + '@' + last_part
-
+    email = Sequence(lambda n: f"user{n}@example.com")
 
 class UsersHaveMadeOutFactory(DjangoModelFactory):
     class Meta:
