23579e58cf3e001b14980ca070594ba12218582f
Tormod Haugland <tormod.haugland@gmail.com>
2020-10-15 18:31:15 +0200

    feat: Add weasyprint dependency
    Also add a banale example of how to do the printing.

--- common/urls.py
+++ common/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.urls import path
 
 from common import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    path("test-pdf-print", views.print_pdf_example)
 ]
