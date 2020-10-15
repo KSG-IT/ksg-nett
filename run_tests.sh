7950083edcffa87c4429fd3a96b569720d4b48e3
Tormod Haugland <tormod.haugland@gmail.com>
2020-10-15 18:23:55 +0200

    fix: Update travis and test running

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 MODULES="api,economy,internal,login,media,organization,quotes,schedules,users"
-pipenv run coverage run --source=$MODULES manage.py test
+poetry run coverage run --source=$MODULES manage.py test
 if [ $? -eq 0 ]
 then
     coverage report
