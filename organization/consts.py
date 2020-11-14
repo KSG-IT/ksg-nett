d2b7c9be39d7e86a357d0176791a39793990d4cc
Alexander Orvik <alexaor@stud.ntnu.no>
2020-11-14 17:23:41 +0100

    feat(organization): move model charfield choices to consts file
    Move KSG_INTERNAL_GROUP and KSG_POSITION tuple pair list to a
    consts.py file in the organization app

---
+++ organization/consts.py
@@ -0,0 +1,47 @@
+
+# Internal groups in KSG
+KSG_INTERNAL_GROUPS = (
+    ("arrangement", "Arrangement"),
+    ("bar", "Bargjengen"),
+    ("bryggeriet", "Daglighallen mikrobryggeri"),
+    ("daglighallen", "Daglighallen"),
+    ("edgar", "Edgar"),
+    ("lychebar", "Lyche Bar"),
+    ("lychekjokken", "Lyche Kjøkken"),
+    ("sprit", "Spritgjengen"),
+    ("styret", "Styret"),
+    ("okonomi", "Økonomi"),
+)
+
+# Positions that can be held while in KSG
+# Note: Should only be available if you are aktiv
+KSG_POSITIONS = (
+    # Arrangement
+    ("arrangementsansvarlig", "Arrangementsansvarlig"),
+    ("arrangementsbartender", "Arrangementsbartender"),
+    # Bargjengen
+    ("barsjef", "Barsjef"),
+    ("bartender", "Bartender"),
+    # Bryggeriet
+    ("bryggfunk", "Bryggfunk"),
+    ("brygger", "Brygger"),
+    # Daglighallen
+    ("daglighallenfunk", "Daglighallenfunk"),
+    ("daglighallenbartender", "Daglighallenbartender"),
+    # Edgar
+    ("kafeansvarlig", "Kaféansvarlig"),
+    ("baristaer", "Barista"),
+    # Lyche bar
+    ("hovmester", "Hovmester"),
+    ("barservitor", "Barservitør"),
+    # Lyche kjøkken
+    ("souschef", "Souschef"),
+    ("kokk", "Kokk"),
+    # Spritgjengen
+    ("spritbarsjef", "Spritbarsjef"),
+    ("spritbartender", "Spritbartender"),
+    # Styret
+    ("styret", "Styret"),
+    # Økonomi
+    ("okonomi", "Økonomi"),
+)
--- organization/models.py
+++ organization/models.py
@@ -3,53 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from users.models import User
-
-# Internal groups in KSG
-KSG_INTERNAL_GROUPS = (
-    ("arrangement", "Arrangement"),
-    ("bar", "Bargjengen"),
-    ("bryggeriet", "Daglighallen mikrobryggeri"),
-    ("daglighallen", "Daglighallen"),
-    ("edgar", "Edgar"),
-    ("lychebar", "Lyche Bar"),
-    ("lychekjokken", "Lyche Kjøkken"),
-    ("sprit", "Spritgjengen"),
-    ("styret", "Styret"),
-    ("okonomi", "Økonomi"),
-)
-
-# Positions that can be held while in KSG
-# Note: Should only be available if you are aktiv
-KSG_POSITIONS = (
-    # Arrangement
-    ("arrangementsansvarlig", "Arrangementsansvarlig"),
-    ("arrangementsbartender", "Arrangementsbartender"),
-    # Bargjengen
-    ("barsjef", "Barsjef"),
-    ("bartender", "Bartender"),
-    # Bryggeriet
-    ("bryggfunk", "Bryggfunk"),
-    ("brygger", "Brygger"),
-    # Daglighallen
-    ("daglighallenfunk", "Daglighallenfunk"),
-    ("daglighallenbartender", "Daglighallenbartender"),
-    # Edgar
-    ("kafeansvarlig", "Kaféansvarlig"),
-    ("baristaer", "Barista"),
-    # Lyche bar
-    ("hovmester", "Hovmester"),
-    ("barservitor", "Barservitør"),
-    # Lyche kjøkken
-    ("souschef", "Souschef"),
-    ("kokk", "Kokk"),
-    # Spritgjengen
-    ("spritbarsjef", "Spritbarsjef"),
-    ("spritbartender", "Spritbartender"),
-    # Styret
-    ("styret", "Styret"),
-    # Økonomi
-    ("okonomi", "Økonomi"),
-)
+from organization.consts import KSG_INTERNAL_GROUPS, KSG_POSITIONS
 
 
 class InternalGroup(models.Model):
