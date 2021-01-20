faaa83395cf088587a48729e3c8dc3d26fe0bbb5
Alexander Orvik <alexaor@stud.ntnu.no>
2021-01-20 08:25:02 +0100

    feat(redis): read env variables redis config

--- ksg_nett/settings.py
+++ ksg_nett/settings.py
@@ -222,8 +222,8 @@ CHANNEL_LAYERS = {
 
 # Redis
 REDIS = {
-    'host': 'localhost',
-    'port': 6379
+    'host': os.environ.get('REDIS_HOST', 'localhost'),
+    'port': os.environ.get('REDIS_PORT', 6379),
 }
 CHAT_STATE_REDIS_DB = 1
 
