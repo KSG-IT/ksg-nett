05c7d4a0b1ce2ca9ceec8f7e3faaf9fdeb3501e9
Alexander Orvik <alexaor@stud.ntnu.no>
2021-01-20 09:06:12 +0100

    feat(setting): set redis-host env variable
    Read the Channels Redis Layer backend host  from env variable
    if it exists. Localhost fallback

--- ksg_nett/settings.py
+++ ksg_nett/settings.py
@@ -214,7 +214,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)]
+            "hosts": [os.environ.get("REDIS_URI", ("localhost", 6379))]
         }
     }
 }
