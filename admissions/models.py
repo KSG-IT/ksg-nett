8299ecf044b6ed6852e6e7bda8213c73b1074db1
Øystein <oystebha@stud.ntnu.no>
2021-01-31 15:12:53 +0100

    made models for applicant and admission

---
+++ admissions/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from common.util import get_semester_year_shorthand
+
+# Create your models here.
+
+#Snakk med Ida og Kristoffer om input på hva de vil ha i en slik modul
+
+#autoexportere bilder fra opptaket i en slide på opptaksmøtet
+#navn, nummer, bilde, studie
+
+class Applicant(models.Model):
+    name = models.CharField(default="", null=False, max_length=100)
+    phone = models.CharField(default="", blank=True, max_length=50)
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    study = models.CharField(default="", blank=True, max_length=100)
+
+
+class Admissions(models.Model):
+    applicants = models.ForeignKey(Applicant)
+    date = models.DateField(blank=True, null=True)
+    admission_closed = models.BooleanField(default=False)
+
+
+    def get_admissions_by_semester(self) -> str:
+        return get_semester_year_shorthand(self.date)
+    
+
+
+
+    
+
