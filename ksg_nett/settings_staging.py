5b91d3e36c4f62c8c8fa59e9d25d8572e597a3fc
Alexander Orvik <alexaor@stud.ntnu.no>
2021-01-31 16:51:31 +0100

    fix(deploy): bucket name setting

--- ksg_nett/settings_staging.py
+++ ksg_nett/settings_staging.py
@@ -58,5 +58,6 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
 AWS_REGION = "eu-west-3"
 AWS_S3_BUCKET_AUTH = False
 AWS_S3_BUCKET_NAME = "ksg-nett-bucket"
+AWS_STORAGE_BUCKET_NAME = "ksg-nett-bucket"
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 MEDIA_URL = f"https://{AWS_S3_BUCKET_NAME}.s3.amazonaws.com/media/"
