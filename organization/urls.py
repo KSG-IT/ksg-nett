6872110edc20ee98a69ae1c8d4f96f3fd184e6e0
Alexander Orvik <alexaor@stud.ntnu.no>
2021-02-07 13:20:02 +0100

    feat(org): bootstrap internal group edit
    - create template for edit internal group
    - bootstrap view to render template
    - add view css file

--- organization/urls.py
+++ organization/urls.py
@@ -4,4 +4,5 @@ from organization import views
 
 urlpatterns = [
     path("internal-groups/<int:internal_group_id>", views.internal_group_detail),
+    path("internal-groups/<int:internal_group_id>/edit", views.internal_group_edit),
 ]
--- organization/views.py
+++ organization/views.py
@@ -12,21 +12,42 @@ def internal_group_detail(request, internal_group_id):
         except InternalGroup.DoesNotExist:
             return render(request, template_name="internal/not_found.html")
 
-        all_internal_groups = InternalGroup.objects.filter(type=InternalGroup.Type.INTERNAL_GROUP).order_by("name")
-        all_interest_groups = InternalGroup.objects.filter(type=InternalGroup.Type.INTEREST_GROUP).order_by("name")
+        all_internal_groups = InternalGroup.objects.filter(
+            type=InternalGroup.Type.INTERNAL_GROUP
+        ).order_by("name")
+        all_interest_groups = InternalGroup.objects.filter(
+            type=InternalGroup.Type.INTEREST_GROUP
+        ).order_by("name")
         ctx = {  # The context returns a list structure of groups the template can loop over to display in the sidebar
             "group_overview": [
                 {
                     "title": InternalGroup.Type.INTERNAL_GROUP.label,
-                    "groups": all_internal_groups
+                    "groups": all_internal_groups,
                 },
                 {
                     "title": InternalGroup.Type.INTEREST_GROUP.label,
-                    "groups": all_interest_groups
+                    "groups": all_interest_groups,
                 },
             ],
             "internal_group": internal_group,
         }
-        return render(request, template_name="organization/internal_groups.html", context=ctx)
+        return render(
+            request, template_name="organization/internal_groups.html", context=ctx
+        )
     else:
         return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@login_required
+def internal_group_edit(request, internal_group_id):
+    if request.method == "GET":
+        return render(
+            request, template_name="organization/internal_group_edit.html", context={}
+        )
+
+    if request.method == "POST":
+        return render(
+            request, template_name="organization/internal_groups.html", context={}
+        )
+
+    return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
