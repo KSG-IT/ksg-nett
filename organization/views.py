be714b344d3d4b2243e530f96cc2ddc082468c8e
Alexander Orvik <alexaor@stud.ntnu.no>
2021-02-10 20:46:14 +0100

    feat(org): bootstrap internal group edit view
    Commit bootstraps remaning functionality and styling for view
    * Template logic with manual rendering of form
    * img tag which shows preview of upload
    * helper function which reloads img with a preview of selected file
    * rename templates for more semantic meaning
    * InternalGroup Modelform in organization/forms.py
    * Register views in organization/urls.py

--- organization/views.py
+++ organization/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404, reverse, redirect
 from organization.models import InternalGroup
+from organization.forms import InternalGroupForm
 from rest_framework import status
+from common.util import compress_image
 
 
 @login_required
-def internal_group_detail(request, internal_group_id):
+def internal_groups_detail(request, internal_group_id):
     if request.method == "GET":
         try:
             internal_group = InternalGroup.objects.get(pk=internal_group_id)
@@ -32,22 +34,47 @@ def internal_group_detail(request, internal_group_id):
             "internal_group": internal_group,
         }
         return render(
-            request, template_name="organization/internal_groups.html", context=ctx
+            request,
+            template_name="organization/internal_groups_detail.html",
+            context=ctx,
         )
     else:
         return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 @login_required
-def internal_group_edit(request, internal_group_id):
+def internal_groups_edit(request, internal_group_id):
+    internal_group = get_object_or_404(InternalGroup, pk=internal_group_id)
+    form = InternalGroupForm(
+        request.POST or None, request.FILES or None, instance=internal_group
+    )
+
+    ctx = {
+        "internal_group": internal_group,
+        "internal_group_form": form,
+    }
     if request.method == "GET":
         return render(
-            request, template_name="organization/internal_group_edit.html", context={}
+            request, template_name="organization/internal_groups_edit.html", context=ctx
         )
 
-    if request.method == "POST":
+    elif request.method == "POST":
+        if form.is_valid():
+            pre_commit_form = form.save(commit=False)
+            pre_commit_form.group_image = compress_image(
+                pre_commit_form.group_image.file, 1200, 900, 80
+            )
+            pre_commit_form.save()
+            return redirect(
+                reverse(
+                    internal_groups_detail,
+                    kwargs={"internal_group_id": internal_group_id},
+                )
+            )
         return render(
-            request, template_name="organization/internal_groups.html", context={}
+            request,
+            template_name="organization/internal_groups_detail.html",
+            context={},
         )
-
-    return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    else:
+        return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
