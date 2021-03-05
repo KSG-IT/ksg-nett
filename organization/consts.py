f1588fee0ee66ba72d65ffa62ac6fa6c705f5c5a
Alexander Orvik <alexao1111@gmail.com>
2021-03-05 15:49:34 +0100

    chore(org): move InternalGroupPosition.Type to file
    Extract the class to a separate consts.py file in the module in
    order to more easily import it other places without conflicts

---
+++ organization/consts.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class InternalGroupPositionType(models.TextChoices):
+    FUNCTIONARY = "functionary", _("Functionary")
+    ACTIVE_FUNCTIONARY_PANG = "active-functionary-pang", _(
+        "Active functionary pang"
+    )
+    OLD_FUNCTIONARY_PANG = "old-functionary-pang", _("Old functionary pang")
+    GANG_MEMBER = "gang-member", _("Gang member")
+    ACTIVE_GANG_MEMBER_PANG = "active-gang-member-pang", _(
+        "Active gang member pang"
+    )
+    OLD_GANG_MEMBER_PANG = "old-gang-member-pang", _("Old gang member pang")
+    INTEREST_GROUP_MEMBER = "interest-group-member", _("Interest group member")
+    HANGAROUND = "hangaround", _("Hangaround")
+    TEMPORARY_LEAVE = "temporary-leave", _("Temporary leave")
--- organization/models.py
+++ organization/models.py
@@ -5,6 +5,7 @@ from typing import Optional
 from django.db import models
 from django.utils import timezone
 from users.models import User
+from organization.consts import InternalGroupPositionType
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -85,21 +86,6 @@ class InternalGroupPosition(models.Model):
     class Meta:
         unique_together = ("name", "internal_group", "type")
 
-    class Type(models.TextChoices):
-        FUNCTIONARY = "functionary", _("Functionary")
-        ACTIVE_FUNCTIONARY_PANG = "active-functionary-pang", _(
-            "Active functionary pang"
-        )
-        OLD_FUNCTIONARY_PANG = "old-functionary-pang", _("Old functionary pang")
-        GANG_MEMBER = "gang-member", _("Gang member")
-        ACTIVE_GANG_MEMBER_PANG = "active-gang-member-pang", _(
-            "Active gang member pang"
-        )
-        OLD_GANG_MEMBER_PANG = "old-gang-member-pang", _("Old gang member pang")
-        INTEREST_GROUP_MEMBER = "interest-group-member", _("Interest group member")
-        HANGAROUND = "hangaround", _("Hangaround")
-        TEMPORARY_LEAVE = "temporary-leave", _("Temporary leave")
-
     name = models.CharField(max_length=32)
     internal_group = models.ForeignKey(
         InternalGroup,
@@ -110,7 +96,7 @@ class InternalGroupPosition(models.Model):
     )
     description = models.CharField(max_length=1024, blank=True, null=True)
     type = models.CharField(
-        max_length=32, choices=Type.choices, null=False, blank=False
+        max_length=32, choices=InternalGroupPositionType.choices, null=False, blank=False
     )
     holders = models.ManyToManyField(
         User,
