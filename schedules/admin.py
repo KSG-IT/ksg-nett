a211e79d9c143515e7b9a8b5ccc1405a9fa5fc70
Alexander Orvik <alexao1111@gmail.com>
2021-04-08 14:15:18 +0200

    feat(schedules): clean up models and admin
    Number of quality of life changes and fixes to names.
    	* Typo in ShiftSlotGroupDayRule
    	* Add FK inlines to model admin
    	* ShiftSlot.group related_name
    	* Add enum choices to ScheduleSlotType.role
    	* Helper properties in ShiftSlotGroup
    	* Black formatting

--- schedules/admin.py
+++ schedules/admin.py
@@ -1,16 +1,68 @@
 from django.contrib import admin
 
-from schedules.models import Schedule, ShiftSlotGroupInterest, ShiftTradeOffer, ShiftTrade, ShiftSlotTemplate, \
-    ShiftSlotDayRule, ShiftSlotGroupTemplate, ScheduleTemplate, Shift, ShiftSlot, ScheduleSlotType, ShiftSlotGroup
+from schedules.models import (
+    Schedule,
+    ShiftSlotGroupInterest,
+    ShiftTradeOffer,
+    ShiftTrade,
+    ShiftSlotTemplate,
+    ShiftSlotGroupDayRule,
+    ShiftSlotGroupTemplate,
+    ScheduleTemplate,
+    Shift,
+    ShiftSlot,
+    ScheduleSlotType,
+    ShiftSlotGroup,
+)
 
-admin.site.register(Schedule)
-admin.site.register(ShiftSlotGroup)
+
+class TemplateInlines(admin.TabularInline):
+    model = ScheduleTemplate
+    extra = 1
+
+
+class ScheduleAdmin(admin.ModelAdmin):
+    inlines = (TemplateInlines,)
+
+
+class ShiftSlotGroupDayRuleInline(admin.TabularInline):
+    model = ShiftSlotGroupDayRule
+    extra = 1
+
+
+class ShiftSlotGroupTemplateAdmin(admin.ModelAdmin):
+    inlines = (ShiftSlotGroupDayRuleInline,)
+
+
+class ShiftSlotInline(admin.TabularInline):
+    model = ShiftSlot
+    extra = 1
+
+
+class ShiftSlotGroupAdmin(admin.ModelAdmin):
+    inlines = (ShiftSlotInline,)
+
+
+class ShiftInline(admin.TabularInline):
+    model = Shift
+
+
+class ShiftSlotAdmin(admin.ModelAdmin):
+    pass
+
+
+class ShiftAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Schedule, ScheduleAdmin)
+admin.site.register(ShiftSlotGroup, ShiftSlotGroupAdmin)
 admin.site.register(ScheduleSlotType)
 admin.site.register(ShiftSlot)
-admin.site.register(Shift)
+admin.site.register(Shift, ShiftAdmin)
 admin.site.register(ScheduleTemplate)
-admin.site.register(ShiftSlotGroupTemplate)
-admin.site.register(ShiftSlotDayRule)
+admin.site.register(ShiftSlotGroupTemplate, ShiftSlotGroupTemplateAdmin)
+admin.site.register(ShiftSlotGroupDayRule)
 admin.site.register(ShiftSlotTemplate)
 admin.site.register(ShiftTrade)
 admin.site.register(ShiftTradeOffer)
--- schedules/models.py
+++ schedules/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from model_utils import Choices
 from model_utils.fields import StatusField
 from model_utils.models import TimeFramedModel
+from django.utils.translation import ugettext_lazy as _
 
 from organization.models import InternalGroup
 from users.models import User
@@ -13,7 +14,8 @@ class Schedule(models.Model):
     I.e. all 'Bodega'-shifts have one schedule, all 'Lyche kjøkken'-shifts have one schedule,
     and so on.
     """
-    name = models.CharField(max_length=100)
+
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return "Schedule %s" % self.name
@@ -22,7 +24,7 @@ class Schedule(models.Model):
         return "Schedule(name=%s)" % self.name
 
     class Meta:
-        verbose_name_plural = 'schedules'
+        verbose_name_plural = "schedules"
 
 
 class ShiftSlotGroup(models.Model):
@@ -31,16 +33,27 @@ class ShiftSlotGroup(models.Model):
     The reason this is interesting is that people who work at the same time might want to connect.
     And we also want to group them in presentational circumstances.
     """
+
     name = models.CharField(max_length=100)
     schedule = models.ForeignKey(
-        Schedule,
-        null=False,
-        blank=False,
-        on_delete=models.CASCADE
+        Schedule, null=False, blank=False, on_delete=models.CASCADE
     )
 
     meet_time = models.DateTimeField(null=False, blank=False)
     start_time = models.DateTimeField(null=False, blank=False)
+
+    @property
+    def get_filled_shifts(self):
+        return self.shift_slots.filter(filled_shift__isnull=False)
+
+    @property
+    def shift_display_name(self):
+        """
+        This property is intended to display the name of the shift slot group in an
+        easily digestible way. If following the assumed naming conventions this would return
+        "Edgar fredag senvakt"
+        """
+        return f"{self.schedule.name} {self.name}".capitalize()
 
     def __str__(self):
         return "ShiftSlotGroup %s for schedule %s" % (self.name, self.schedule.name)
@@ -49,8 +62,13 @@ class ShiftSlotGroup(models.Model):
         return "ShiftSlotGroup(name=%s, schedule=%s)" % (self.name, self.schedule.name)
 
     class Meta:
-        verbose_name_plural = 'shift slot groups'
-        unique_together = (('name', 'schedule',),)
+        verbose_name_plural = "shift slot groups"
+        unique_together = (
+            (
+                "name",
+                "schedule",
+            ),
+        )
 
 
 class ScheduleSlotType(models.Model):
@@ -58,53 +76,64 @@ class ScheduleSlotType(models.Model):
     This model represents a type of slot that can be employed in a schedules shifts.
     This would typically be values like "Hovmester", "Barsjef", etc.
     """
+
     name = models.CharField(max_length=100, null=False, blank=False)
     schedule = models.ForeignKey(
-        Schedule,
-        null=False,
-        blank=False,
-        on_delete=models.CASCADE
+        Schedule, null=False, blank=False, on_delete=models.CASCADE
     )
+
+    class Role(models.TextChoices):
+        FUNCTIONARY = "functionary", _("Functionary")
+        GANG_MEMBER = "gang-member", _("Gang member")
 
     # This model describes what type of role is associated with
     # the slot type. For instance is Hovmester a "funkevakt", and Barservitør a "gjengisvakt".
     # TODO: Add choices when the new organization branch is merged
-    role = models.CharField(max_length=32)
+    role = models.CharField(max_length=32, choices=Role.choices)
 
     # This fields represents which groups will be used in an automatic generation
     # of a schedule
-    standard_groups = models.ManyToManyField(InternalGroup, blank=True)
+    standard_groups = models.ManyToManyField(
+        InternalGroup, blank=True
+    )  # what is this for even?
 
     def __str__(self):
         return "ScheduleSlotType %s for schedule %s" % (self.name, self.schedule.name)
 
     def __repr__(self):
-        return "ScheduleSlotType(name=%s, schedule=%s)" % (self.name, self.schedule.name)
+        return "ScheduleSlotType(name=%s, schedule=%s)" % (
+            self.name,
+            self.schedule.name,
+        )
 
     class Meta:
-        verbose_name_plural = 'schedule slot types'
-        unique_together = (('name', 'schedule',),)
+        verbose_name_plural = "schedule slot types"
+        unique_together = (
+            (
+                "name",
+                "schedule",
+            ),
+        )
 
 
 class ShiftSlot(models.Model):
     """
     The ShiftSlot model represents a shift that is due to be or is filled by a person.
     """
+
     start = models.TimeField(blank=False, null=False)
     end = models.TimeField(blank=False, null=False)
 
     type = models.ForeignKey(
-        ScheduleSlotType,
-        null=False,
-        blank=False,
-        on_delete=models.CASCADE
+        ScheduleSlotType, null=False, blank=False, on_delete=models.CASCADE
     )
 
     group = models.ForeignKey(
         ShiftSlotGroup,
         null=False,
         blank=False,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="shift_slots",
     )
 
     def __str__(self):
@@ -112,7 +141,7 @@ class ShiftSlot(models.Model):
             self.type.name,
             self.group.name,
             self.start.strftime("%H:%M"),
-            self.end.strftime("%H:%M")
+            self.end.strftime("%H:%M"),
         )
 
     def __repr__(self):
@@ -120,11 +149,11 @@ class ShiftSlot(models.Model):
             self.type.name,
             self.group.name,
             self.start.strftime("%H:%M"),
-            self.end.strftime("%H:%M")
+            self.end.strftime("%H:%M"),
         )
 
     class Meta:
-        verbose_name_plural = 'Shift slots'
+        verbose_name_plural = "Shift slots"
 
 
 class Shift(models.Model):
@@ -132,19 +161,15 @@ class Shift(models.Model):
     The Shift model represents a shift-slot filled by
     a person.
     """
-    user = models.ForeignKey(
-        User,
-        null=False,
-        blank=False,
-        on_delete=models.CASCADE
-    )
+
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
 
     slot = models.OneToOneField(
         ShiftSlot,
         null=False,
         blank=False,
-        related_name='filled_shift',
-        on_delete=models.CASCADE
+        related_name="filled_shift",
+        on_delete=models.CASCADE,
     )
 
     def __str__(self):
@@ -154,7 +179,7 @@ class Shift(models.Model):
         return "Shift(slot=%d, user=%s)" % (self.slot.id, self.user.first_name)
 
     class Meta:
-        verbose_name_plural = 'shifts'
+        verbose_name_plural = "shifts"
 
 
 class ScheduleTemplate(models.Model):
@@ -162,24 +187,30 @@ class ScheduleTemplate(models.Model):
     This model contains a template for a schedules standard week. A schedule may have
     several templates.
     """
+
     name = models.CharField(max_length=100)
 
     schedule = models.ForeignKey(
-        Schedule,
-        blank=False,
-        null=False,
-        on_delete=models.CASCADE
+        Schedule, blank=False, null=False, on_delete=models.CASCADE
     )
 
     def __str__(self):
         return "Template %s for schedule %s" % (self.name, self.schedule.name)
 
     def __repr__(self):
-        return "ScheduleTemplate(name=%s, schedule=%s)" % (self.name, self.schedule.name)
+        return "ScheduleTemplate(name=%s, schedule=%s)" % (
+            self.name,
+            self.schedule.name,
+        )
 
     class Meta:
-        verbose_name_plural = 'schedule templates'
-        unique_together = (('name', 'schedule',),)
+        verbose_name_plural = "schedule templates"
+        unique_together = (
+            (
+                "name",
+                "schedule",
+            ),
+        )
 
 
 class ShiftSlotGroupTemplate(models.Model):
@@ -189,83 +220,89 @@ class ShiftSlotGroupTemplate(models.Model):
     The model contains some additional information over a ShiftSlot, namely the rules
     for when the shift slot is supposed to occur.
     """
+
     name = models.CharField(max_length=100)
 
     schedule_template = models.ForeignKey(
-        ScheduleTemplate,
-        blank=False,
-        null=False,
-        on_delete=models.CASCADE
+        ScheduleTemplate, blank=False, null=False, on_delete=models.CASCADE
     )
 
     meet_time = models.TimeField(blank=False, null=False)
     start_time = models.TimeField(blank=False, null=False)
 
     def __str__(self):
-        return "Template for ShiftSlotGroup %s for schedule-template %s" % (self.name, self.schedule_template.name)
+        return "Template for ShiftSlotGroup %s for schedule-template %s" % (
+            self.name,
+            self.schedule_template.name,
+        )
 
     def __repr__(self):
-        return "ShiftSlotGroupTemplate(name=%s, schedule_template=%s" % (self.name, self.schedule_template.name)
+        return "ShiftSlotGroupTemplate(name=%s, schedule_template=%s" % (
+            self.name,
+            self.schedule_template.name,
+        )
 
     class Meta:
-        verbose_name_plural = 'shift slot group templates'
+        verbose_name_plural = "shift slot group templates"
 
 
-class ShiftSlotDayRule(models.Model):
+class ShiftSlotGroupDayRule(models.Model):
     """
     This model represents a rule for what days a ShiftSlotGroup can occur
     """
+
     STATUS = Choices(
-        ('mo', 'Monday'),
-        ('tu', 'Tuesday'),
-        ('we', 'Wednesday'),
-        ('th', 'Thursday'),
-        ('fr', 'Friday'),
-        ('sa', 'Saturday'),
-        ('su', 'Sunday'),
-        ('wk', 'Weekdays'),
-        ('ed', 'Weekends'),  # Friday, Saturday
-        ('fu', 'Full weekends'),  # Friday, Saturday, Sunday
+        ("mo", "Monday"),
+        ("tu", "Tuesday"),
+        ("we", "Wednesday"),
+        ("th", "Thursday"),
+        ("fr", "Friday"),
+        ("sa", "Saturday"),
+        ("su", "Sunday"),
+        ("wk", "Weekdays"),
+        ("ed", "Weekends"),  # Friday, Saturday
+        ("fu", "Full weekends"),  # Friday, Saturday, Sunday
     )
 
     rule = StatusField(max_length=2)
 
-    shift_slot_template = models.ForeignKey(
-        ShiftSlotGroupTemplate,
-        related_name='day_rules',
-        on_delete=models.CASCADE
+    shift_slot_group_template = models.ForeignKey(
+        ShiftSlotGroupTemplate, related_name="day_rules", on_delete=models.CASCADE
     )
 
     def __str__(self):
-        return "Rule %s for shift slot group %s" % (self.get_rule_display(), self.shift_slot_template.name)
+        return f"Rule {self.get_rule_display()} for shift slot group {self.shift_slot_group_template.name} for {self.shift_slot_group_template.schedule_template.schedule}"
 
     def __repr__(self):
-        return "ShiftSlotDayRule(rule=%s, shift_slot_template=%s)" % (self.get_rule_display(), self.shift_slot_template)
+        return "ShiftSlotGroupDayRule(rule=%s, shift_slot_group_template=%s)" % (
+            self.get_rule_display(),
+            self.shift_slot_group_template,
+        )
 
     class Meta:
-        verbose_name_plural = 'Shift slot day rules'
-        unique_together = (('rule', 'shift_slot_template',),)
+        verbose_name_plural = "Shift slot group day rules"
+        unique_together = (
+            (
+                "rule",
+                "shift_slot_group_template",
+            ),
+        )
 
 
 class ShiftSlotTemplate(models.Model):
     """
     This model represents a template for a ShiftSlot.
     """
+
     start = models.TimeField(blank=False, null=False)
     end = models.TimeField(blank=False, null=False)
 
     type = models.ForeignKey(
-        ScheduleSlotType,
-        null=False,
-        blank=False,
-        on_delete=models.CASCADE
+        ScheduleSlotType, null=False, blank=False, on_delete=models.CASCADE
     )
 
     group = models.ForeignKey(
-        ShiftSlotGroupTemplate,
-        null=False,
-        blank=False,
-        on_delete=models.CASCADE
+        ShiftSlotGroupTemplate, null=False, blank=False, on_delete=models.CASCADE
     )
 
     def __str__(self):
@@ -273,7 +310,7 @@ class ShiftSlotTemplate(models.Model):
             self.type,
             self.group.name,
             self.start.strftime("%H:%M"),
-            self.end.strftime("%H:%M")
+            self.end.strftime("%H:%M"),
         )
 
     def __repr__(self):
@@ -281,11 +318,11 @@ class ShiftSlotTemplate(models.Model):
             self.type,
             self.group.name,
             self.start.strftime("%H:%M"),
-            self.end.strftime("%H:%M")
+            self.end.strftime("%H:%M"),
         )
 
     class Meta:
-        verbose_name_plural = 'Shift slot templates'
+        verbose_name_plural = "Shift slot templates"
 
 
 class ShiftTrade(models.Model):
@@ -302,42 +339,46 @@ class ShiftTrade(models.Model):
         User,
         blank=False,
         null=False,
-        related_name='shifts_offered',
-        on_delete=models.CASCADE
+        related_name="shifts_offered",
+        on_delete=models.CASCADE,
     )
     shift_offer = models.ForeignKey(
         Shift,
         blank=False,
         null=False,
-        related_name='offered_in_trades',
-        on_delete=models.CASCADE
+        related_name="offered_in_trades",
+        on_delete=models.CASCADE,
     )
 
     taker = models.ForeignKey(
         User,
         blank=True,
         null=True,
-        related_name='shifts_taken',
-        on_delete=models.SET_NULL
+        related_name="shifts_taken",
+        on_delete=models.SET_NULL,
     )
     shift_taker = models.ForeignKey(
         Shift,
         blank=True,
         null=True,
-        related_name='taken_in_trades',
-        on_delete=models.SET_NULL
+        related_name="taken_in_trades",
+        on_delete=models.SET_NULL,
     )
     signed_off_by = models.ForeignKey(
         User,
         blank=True,
         null=True,
-        related_name='shift_trades_signed_off',
-        on_delete=models.SET_NULL
+        related_name="shift_trades_signed_off",
+        on_delete=models.SET_NULL,
     )
 
     @property
     def valid(self):
-        return self.signed_off_by is not None and self.taker is not None and self.shift_taker is not None
+        return (
+            self.signed_off_by is not None
+            and self.taker is not None
+            and self.shift_taker is not None
+        )
 
     @property
     def committed(self):
@@ -350,7 +391,7 @@ class ShiftTrade(models.Model):
         return "ShiftTrade(offeror=%s)" % (self.offeror.first_name,)
 
     class Meta:
-        verbose_name_plural = 'Shift trade'
+        verbose_name_plural = "Shift trade"
 
 
 class ShiftTradeOffer(models.Model):
@@ -365,23 +406,23 @@ class ShiftTradeOffer(models.Model):
         ShiftTrade,
         blank=False,
         null=False,
-        related_name='counter_offers',
-        on_delete=models.CASCADE
+        related_name="counter_offers",
+        on_delete=models.CASCADE,
     )
 
     offeror = models.ForeignKey(
         User,
         blank=False,
         null=False,
-        related_name='shift_offers',
-        on_delete=models.CASCADE
+        related_name="shift_offers",
+        on_delete=models.CASCADE,
     )
     shift_offer = models.ForeignKey(
         Shift,
         blank=False,
         null=False,
-        related_name='offered_to_shifts',
-        on_delete=models.CASCADE
+        related_name="offered_to_shifts",
+        on_delete=models.CASCADE,
     )
 
     accepted = models.BooleanField(default=False, null=False, blank=False)
@@ -389,17 +430,17 @@ class ShiftTradeOffer(models.Model):
     def __str__(self):
         return "Counter trade by %s to shift offered by %s" % (
             self.offeror.first_name,
-            self.shift_trade.offeror.first_name
+            self.shift_trade.offeror.first_name,
         )
 
     def __repr__(self):
         return "ShiftTradeOffer(from=%s, as_counter_to=%s)" % (
             self.offeror.first_name,
-            self.shift_trade.offeror.first_name
+            self.shift_trade.offeror.first_name,
         )
 
     class Meta:
-        verbose_name_plural = 'Shift trade offer'
+        verbose_name_plural = "Shift trade offer"
 
 
 class ShiftSlotGroupInterest(models.Model):
@@ -408,31 +449,27 @@ class ShiftSlotGroupInterest(models.Model):
     ensure that the user is sent notifications when shifts are up for trade in the given
     group.
     """
+
     shift_group = models.ForeignKey(
-        ShiftSlotGroup,
-        null=False,
-        blank=False,
-        on_delete=models.CASCADE
+        ShiftSlotGroup, null=False, blank=False, on_delete=models.CASCADE
     )
 
     user = models.ForeignKey(
-        User,
-        related_name='shifts_interests',
-        on_delete=models.CASCADE
+        User, related_name="shifts_interests", on_delete=models.CASCADE
     )
 
     def __str__(self):
         return "Interest in shift group %s by %s" % (
             self.shift_group.name,
-            self.user.first_name
+            self.user.first_name,
         )
 
     def __repr__(self):
         return "ShiftSlotGroupInterest(shift_group=%s, user=%s)" % (
             self.shift_group.name,
-            self.user.first_name
+            self.user.first_name,
         )
 
     class Meta:
-        verbose_name_plural = 'Shift slot group interest'
-        unique_together = (('shift_group', 'user'),)
+        verbose_name_plural = "Shift slot group interest"
+        unique_together = (("shift_group", "user"),)
