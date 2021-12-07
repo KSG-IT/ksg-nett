e5caf9ccc5dffc8c509491e5f7d65186a78e7e19
Øystein <oystebha@stud.ntnu.no>
2021-12-07 18:40:58 +0100

    feat(events): changes to model fields, add ordering to all_events resolver

--- events/models.py
+++ events/models.py
@@ -11,11 +11,11 @@ class Event(models.Model):
         verbose_name_plural = "Events"
 
     title = models.CharField(unique=True, max_length=80)
-    description = models.CharField(max_length=1024)
-    date = models.DateField(blank=True, null=True)
+    description = models.TextField(max_length=1024)
+    date = models.DateTimeField(blank=True, null=True)
 
     max_participants = models.IntegerField(default=10)
-    participants = models.ManyToManyField(User, blank=True)
+    participants = models.ManyToManyField(User, blank=True, related_name="attended_events")
 
     allowed_to_join = models.ManyToManyField(InternalGroupPosition, blank=True)
 
