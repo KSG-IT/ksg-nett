e7d703e981de82cf145a9cb3bf8a39b0a72e424f
Alexander Orvik <alexao1111@gmail.com>
2022-10-03 14:40:23 +0200

    chore(schedules): minor textchoice cleanup
    	* Rename Barvakt -> Socivakt
    	* Order of choices

--- schedules/models.py
+++ schedules/models.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.db import models
 from model_utils.models import TimeFramedModel
 from django.utils.translation import ugettext_lazy as _
@@ -64,23 +62,24 @@ class ShiftSlot(models.Model):
         verbose_name_plural = "Shift slots"
 
     class RoleOption(models.TextChoices):
-        UGLE = ("UGLE", "Ugle")
-        BRANNVAKT = ("BRANNVAKT", "Brannvakt")
         BARISTA = ("BARISTA", "Barista")
         KAFEANSVARLIG = ("KAFEANSVARLIG", "Kaféansvarlig")
         BARSERVITOR = ("BARSERVITOR", "Barservitør")
         HOVMESTER = ("HOVMESTER", "Hovmester")
         KOKK = ("KOKK", "Kokk")
         SOUSCHEF = ("SOUSCHEF", "Souschef")
-        RYDDEVAKT = ("RYDDEVAKT", "Ryddevakt")
         ARRANGEMENTBARTENDER = ("ARRANGEMENTBARTENDER", "Arrangementbartender")
         ARRANGAMENTANSVARLIG = ("ARRANGEMENTANSVARLIG", "Arrangementansvarlig")
         BRYGGER = ("BRYGGER", "Brygger")
         BARTENDER = ("BARTENDER", "Bartender")
         BARSJEF = ("BARSJEF", "Barsjef")
-        BARVAKT = ("BARVAKT", "Barvakt")  # Soci
         SPRITBARTENDER = ("SPRITBARTENDER", "Spritbartender")
         SPRITBARSJEF = ("SPRITBARSJEF", "Spritbarsjef")
+
+        UGLE = ("UGLE", "Ugle")
+        BRANNVAKT = ("BRANNVAKT", "Brannvakt")
+        RYDDEVAKT = ("RYDDEVAKT", "Ryddevakt")
+        SOCIVAKT = ("SOCIVAKT", "Socivakt")
 
     shift = models.ForeignKey(Shift, on_delete=models.CASCADE, related_name="slots")
     user = models.ForeignKey(
