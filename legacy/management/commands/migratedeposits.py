4e8e689b12bd864b4bd0d44435dca37d6454270d
Alexander Orvik <alexao1111@gmail.com>
2022-11-01 17:49:05 +0100

    chore(legacy): misc migration fixes

--- legacy/management/commands/migratedeposits.py
+++ legacy/management/commands/migratedeposits.py
@@ -4,17 +4,8 @@ from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
 from django.utils import timezone
 
-from legacy.models import Sitater, Innskudd
+from legacy.models import Innskudd
 from economy.models import Deposit, SociBankAccount
-
-"""
-person = models.ForeignKey("Personer", models.DO_NOTHING, db_column="person")
-registrert = models.DateTimeField()
-penger = models.FloatField()
-type = models.CharField(max_length=1)
-godkjent = models.BooleanField()
-kommentar = models.TextField(blank=True, null=True)
-"""
 
 
 class Command(BaseCommand):
@@ -33,8 +24,8 @@ class Command(BaseCommand):
                 f"Migrating {legacy_deposits.count()} legacy deposits to new table"
             )
         )
-        with transaction.atomic():
-            for deposit in legacy_deposits:
+        for deposit in legacy_deposits:
+            with transaction.atomic():
 
                 # ToDo this timezone aware logic. Look at the timezone legacy objects
                 aware_datetime = deposit.registrert
@@ -49,9 +40,7 @@ class Command(BaseCommand):
                 Deposit.objects.get_or_create(
                     amount=deposit.penger,
                     created_at=aware_datetime,
-                    account=SociBankAccount.objects.filter(
-                        user__sg_id=deposit.person.id
-                    ).first(),
+                    account=SociBankAccount.objects.get(user__sg_id=deposit.person.id),
                     signed_off_time=aware_datetime,
                     description=deposit.kommentar,
                     migrated_from_sg=True,
