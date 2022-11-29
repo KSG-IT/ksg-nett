2de79c169ceb0b27617621e8c9d7a3cea403d335
Alexander Orvik <alexao1111@gmail.com>
2022-11-29 21:33:19 +0100

    chore: remove sentry from local setup

--- ksg_nett/settings.py
+++ ksg_nett/settings.py
@@ -14,8 +14,6 @@ import os
 from datetime import timedelta
 import warnings
 from corsheaders.defaults import default_headers
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
