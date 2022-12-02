f2bfc7c00c9bd30ec5413a3045f8c5bcf9f27ca6
Alexander Orvik <alexao1111@gmail.com>
2022-12-02 15:47:50 +0100

    feat(summaries): sanitize summary content in node resolver
    By sanitizing the output in the resolver and saving summary
    content as is in the database we can ensure that any future changes
    in bleach will be applied at all time when upgrading the library

--- summaries/schema.py
+++ summaries/schema.py
@@ -1,4 +1,5 @@
 import graphene
+import bleach
 from graphene import Node
 from graphene_django import DjangoObjectType
 from graphene_django_cud.mutations import (
@@ -8,6 +9,8 @@ from graphene_django_cud.mutations import (
 )
 from graphene_django.filter import DjangoFilterConnectionField
 
+from common.consts import BLEACH_ALLOWED_TAGS
+from common.decorators import gql_login_required
 from summaries.models import Summary
 from users.schema import UserNode
 from summaries.filters import SummaryFilter
@@ -25,6 +28,11 @@ class SummaryNode(DjangoObjectType):
     def get_node(cls, info, id):
         return Summary.objects.get(pk=id)
 
+    contents = graphene.String()
+
+    def resolve_contents(self: Summary, info, *args, **kwargs):
+        return bleach.clean(self.contents, tags=BLEACH_ALLOWED_TAGS)
+
     def resolve_participants(self: Summary, info, *args, **kwargs):
         return self.participants.all()
 
@@ -35,6 +43,7 @@ class SummaryQuery(graphene.ObjectType):
         SummaryNode, filterset_class=SummaryFilter
     )
 
+    @gql_login_required()
     def resolve_all_summaries(self, info, *args, **kwargs):
         return Summary.objects.all().order_by("-date")
 
