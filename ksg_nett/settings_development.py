8176f4c04856637ae268abbfd8b67732c3a8e165
Alexander Orvik <alexao1111@gmail.com>
2022-12-03 15:37:57 +0100

    chore(settings): sentry init bug

--- ksg_nett/settings_development.py
+++ ksg_nett/settings_development.py
@@ -43,10 +43,3 @@ DATABASES = {
         "PORT": os.environ.get("DB_PORT_LEGACY"),
     },
 }
-
-sentry_sdk.init(
-    dsn=os.environ.get("SENTRY_DSN", ""),
-    integrations=[DjangoIntegration()],
-    traces_sample_rate=1.0,
-    send_default_pii=True,
-)
