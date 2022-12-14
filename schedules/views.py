2d1a2fddc3d9e9ee33a6a60ec3559589c6ed7df5
Alexander Orvik <alexao1111@gmail.com>
2022-12-14 16:34:24 +0100

    feat(schedules): import ical endpoint
    	* Empty schedule if no ical_token match
    	* Add interview object name if exists
    	* Add other interviewers as attendee's

--- schedules/views.py
+++ schedules/views.py
@@ -5,9 +5,12 @@ from .models import Shift
 
 
 def get_schedule_from_ical_token(request, ical_token):
-    user = User.objects.get(ical_token=ical_token)
-    user_shifts = Shift.objects.filter(slots__user=user)
     cal = Calendar()
+    user = User.objects.filter(ical_token=ical_token).first()
+    if not user:
+        return HttpResponse(cal.to_ical(), content_type="text/calendar")
+
+    user_shifts = Shift.objects.filter(slots__user=user)
 
     for shift in user_shifts:
         event = Event()
@@ -20,10 +23,18 @@ def get_schedule_from_ical_token(request, ical_token):
     interviews = user.interviews_attended.all()
     for interview in interviews:
         event = Event()
-        event.add("summary", "Intervju")
         event.add("dtstart", interview.interview_start)
         event.add("dtend", interview.interview_end)
         event.add("location", interview.location.name)
+
+        if hasattr(interview, "applicant"):
+            event.add("summary", f"Intervju: {interview.applicant.get_full_name}")
+        else:
+            event.add("summary", "Tomt intervju")
+
+        for interviewer in interview.interviewers.all().exclude(id=user.id):
+            event.add("attendee", interviewer.email)
+
         cal.add_component(event)
 
     return HttpResponse(cal.to_ical(), content_type="text/calendar")
