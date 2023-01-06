6c98b7fd5653ec9aa877f8474a4d1438cebc89a3
Alexander Orvik <alexao1111@gmail.com>
2023-01-06 17:27:30 +0100

    fix(legacy): correct user model field use

--- legacy/management/commands/migrateusers.py
+++ legacy/management/commands/migrateusers.py
@@ -105,7 +105,7 @@ class Command(BaseCommand):
                     )
                     card_uuid = None
 
-                new_user.profile_image = f"profiles/{user.sg_ud}.jpg"
+                new_user.profile_image = f"profiles/{user.sg_id}.jpg"
 
                 SociBankAccount.objects.create(
                     user=new_user,
