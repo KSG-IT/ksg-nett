d65ac2204dfaadf6dc9b716aa943e461049cdf90
Alexander Orvik <alexao1111@gmail.com>
2023-02-10 02:40:42 +0100

    feat(economy): add stripe payment intent success webhook handling

--- economy/urls.py
+++ economy/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from . import views
 from django.views.decorators.csrf import csrf_exempt
 
-urlpatterns = [
-    path("print-list", csrf_exempt(views.download_soci_session_list_pdf)),
-]
+urlpatterns = [path("stripe-webhook", csrf_exempt(views.stripe_webhook))]
