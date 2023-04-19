403cfe047ebf2b579df45b057929f4c4f3062e7a
Alexander Orvik <alexao1111@gmail.com>
2023-04-19 12:12:27 +0200

    chore(deps): remove unused boto3 dep

--- ksg_nett/storage_backends.py
+++
@@ -1,7 +0,0 @@
-from storages.backends.s3boto3 import S3Boto3Storage
-
-
-class PublicMediaStorage(S3Boto3Storage):
-    location = "media"
-    default_acl = "public-read"
-    file_overwrite = False
