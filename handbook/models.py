52cd19c9db7c5b4cde743628c291a32201e88f80
Alexander Orvik <alexao1111@gmail.com>
2026-07-24 17:51:40 +0300

    feat(handbook): add highlighted field to document
    Intended to allow certain documents to be highlighted to he app
    navbar

--- handbook/models.py
+++ handbook/models.py
@@ -8,6 +8,7 @@ class Document(models.Model):
 
     name = models.CharField(max_length=100)
     content = models.TextField()
+    highlighted = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
